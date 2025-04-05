b47d574f6405bbb9b0f1dd0f74be80e532623314
ashir-valjee <ashir@eco-motive.com>
2025-04-05 20:51:16 +0100

    second commit

--- client/app2.js
+++ client/app2.js
@@ -17,13 +17,25 @@ console.log(myCommentContainer);
 async function renderComments2(n) {
   const allComments = await getComments();
   for (let i = 0; i < n; i++) {
+    const commentContainer = document.createElement("div");
     const myComment = document.createElement("p");
+    const postId = document.createElement("p");
+    const userName = document.createElement("p");
 
     const bob2 = allComments[i].body;
     console.log(bob2);
+    postId.textContent = `postID ${allComments[i].postId}`;
+    userName.textContent = allComments[i].name;
     myComment.textContent = bob2;
-    myCommentContainer.appendChild(myComment);
+
+    commentContainer.append(postId);
+    commentContainer.append(userName);
+    commentContainer.append(myComment);
+
+    myCommentContainer.appendChild(commentContainer);
+    commentContainer.classList.add("individual-comment");
+    myCommentContainer.classList.add("comment-div");
   }
 }
 
-renderComments2(1);
+renderComments2(10);
